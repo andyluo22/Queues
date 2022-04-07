c43edadd991aaabaefa3d82c2cec55f08aab7245
Andyl <andy.luo22@hotmail.com>
2022-04-06 22:18:52 -0700

    Attempted Using ResizingArray on RandomizedQueue

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,16 +1,17 @@
 import java.util.Iterator;
 
-//Probably want to use resizing array and then shuffle every cycle and remove the first or last part of the array
+//Probably want to use resizing array and then just select a random number uniformly and access that position/index in the array
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static final int INIT_CAPACITY = 8;
+
     private int size;
-    private DLLNode header;
-    private DLLNode trailer;
+    private Item[] a;
+
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-        trailer = new DLLNode(null, null, null);
-        header = new DLLNode(null, null, trailer);
-        trailer.setPrev(header);
+        a = (Item[]) new Object[INIT_CAPACITY];
+        size = 0;
     }
 
     // is the randomized queue empty?
@@ -20,16 +21,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
-        return;
+        if (size == a.length) {
+            resize(2 * a.length);
+        }
+        a[size] = item;
+        size++;
     }
 
     // remove and return a random item
     public Item dequeue() {
+
         return null;
     }
 
@@ -38,9 +44,28 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         return null;
     }
 
+    // resize the underlying array holding the elements
+    private void resize(int capacity) {
+        assert capacity >= size;
+
+        // textbook implementation
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            copy[i] = a[i];
+        }
+        a = copy;
+
+        // alternative implementation
+        // a = java.util.Arrays.copyOf(a, capacity);
+    }
+
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        return RandomizedQueueIterator;
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+
     }
 
     // unit testing (required)
